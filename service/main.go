6b2fe7ccb7566188d9f7c0ab5c71d9d9172f4051
Dmitry <dsalahutdinov@gmail.com>
2021-12-09 09:36:14 +0500

    favorites endpoint for golang service

--- service/main.go
+++ service/main.go
@@ -3,26 +3,36 @@ package main
 import (
     "fmt"
     "net/http"
+    "encoding/json"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-
     fmt.Fprintf(w, "hello\n")
 }
 
-func headers(w http.ResponseWriter, req *http.Request) {
+type FavoriteItem struct {
+    ProductID string `json:"product_id"`
+}
 
-    for name, headers := range req.Header {
-        for _, h := range headers {
-            fmt.Fprintf(w, "%v: %v\n", name, h)
-        }
+func favorites(w http.ResponseWriter, req *http.Request) {
+  userID := req.Header.Get("X-Auth-Identity")
+  if (userID != "") {
+    data :=  map[string][]FavoriteItem {
+      "123": []FavoriteItem{  FavoriteItem{ ProductID: "1"}, FavoriteItem {ProductID: "2"}},
+      "234": []FavoriteItem{  FavoriteItem{ ProductID: "1"}, FavoriteItem {ProductID: "3"}},
+      "345": []FavoriteItem{  FavoriteItem{ ProductID: "1"}, FavoriteItem {ProductID: "4"}},
     }
+    json.NewEncoder(w).Encode(data[userID])
+
+  } else {
+    w.WriteHeader(http.StatusForbidden)
+  }
 }
 
 func main() {
 
     http.HandleFunc("/hello", hello)
-    http.HandleFunc("/headers", headers)
+    http.HandleFunc("/favorites", favorites)
 
     http.ListenAndServe(":8383", nil)
 }
